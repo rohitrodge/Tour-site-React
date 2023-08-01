d850d215b32a4bebd63b0105e60d6d1f473080cc
rohitrodge <rohitrodge27@gmail.com>
2023-08-01 15:08:05 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,115 @@
+import Kedarnath from './assests/Kedarnath.jpg';
+import baiyanath from './assests/baidyanath.jpg';
+import Ghrish from './assests/Ghrish.jpg';
+import mallikarjun from './assests/mallikarjun.jpg';
+import omkareshwar from './assests/omkareshwar.jpg';
+import rameshwaram from './assests/rameshwaram.jpg';
+import somnath from './assests/somnath.jpg';
+import vishwanath from './assests/vishwanath.jpg';
+import bhim from './assests/bhim.jpg';
+import Nageshwar from './assests/Nageshwar.jpg';
+import mahakal from './assests/mahakal.jpg';
+import trim from './assests/trim.jpg';
+
+
+const data = [
+    {
+        id:"1",
+        name :"Kedarnath",
+        info : "Hindu tradition, it is believed that Lord Shiva manifested in form of Jyotirlingam or the cosmic light. There are 12 such Jyotirlingas and Kedarnath is highest among them. This magnificent temple is ancient and constructed over a thousand years ago by Jagad Guru Adi Shankaracharya. It is located in the Rudra Himalaya range of Uttarakhand State. It is situated at an altitude of 3,581 mt & it is a 16 km trek from the nearest place of Gaurikund. The Kedarnath temple is built of massive stone slabs over a large rectangular platform. ",
+        image : Kedarnath,
+        price : "27098",
+    },
+
+{
+    id:"2",
+        name :"Trimbakeshwar",
+        info : "Trimbakeshwar Temple of Lord Shiva is a famous temple in Nashik. The jyotirlinga, in Trimbakeshwar Temple, is only one of the 12 jyotirlingas in India. It not only honors Lord Shiva but also the other two gods in the Holy Trinity (Lord Vishnu and Lord Brahma).",
+        image : trim,
+        price : "18099",
+},
+
+{
+    id:"3",
+        name :"Mahakaleshwar",
+        info : "Mahakaleshwar Jyotirlinga is a Hindu temple dedicated to Shiva and is one of the twelve Jyotirlingas, shrines which are said to be the most sacred abodes of Shiva. It is located in the ancient city of Ujjain in the state of Madhya Pradesh, India. The temple is situated on the side of the holy river Shipra.",
+        image : mahakal,
+        price : "35980",
+},
+
+
+{
+    id:"4",
+        name :"Omkareshwar",
+        info : "Omkareshwar is a Hindu temple dedicated to Shiva, located in Mandhata, nearby Khandwa city in Khandwa district of the Indian state of Madhya Pradesh. It is one of the 12 revered Jyotirlinga shrines of Shiva",
+        image : omkareshwar,
+        price : "77550",
+},
+
+
+{
+    id:"5",
+        name :"Mallikarjuna",
+        info : "Sri Bhramaramba Mallikarjuna Temple or Srisailam Temple is a Hindu temple dedicated to the deities Shiva and Parvati, located at Srisailam in the Indian state of Andhra Pradesh.",
+        image : mallikarjun,
+        price : "65785",
+},
+
+{
+    id:"6",
+        name :"Bhimashankar",
+        info : "The Bhimashankar Mandir is a Shiva mandir situated in its eponymous village, Bhimashankar, in Pune district of Maharashtra. It is a key pilgrimage centre and one of the 12 Jyotirlinga in the world. The temple's Shiva lingam is one of the three Jyotirlinga of Maharashtra. The mandir is situated on a mountain.",
+        image : bhim,
+        price : "36528",
+},
+
+{
+    id:"7",
+        name :"Somnath",
+        info : "The Somnath temple, also called Somanātha temple or Deo Patan, is a Hindu temple located in Prabhas Patan, Veraval in Gujarat, India. It is one of the most sacred pilgrimage sites for Hindus and is the first among the twelve jyotirlinga shrines of Shiva.",
+        image : somnath,
+        price : "45632",
+},
+
+{
+    id:"8 ",
+        name :"Kashi Vishwanath",
+        info : "The Kashi Vishwanath Temple is a famous Hindu temple dedicated to Shiva. It is located in Vishwanath Gali, near Varanasi, Uttar Pradesh, India. The temple is one of the most sacred pilgrimage sites for Shaivites and is among the twelve Jyotirlinga, shrines which are said to be the most sacred abodes of Shiva. ",
+        image : vishwanath,
+        price : "22750",
+},
+
+{
+    id:"9",
+        name :"Nageshwar",
+        info : "Nageshwara Jyotirlinga is one of the twelve Jyotirlings, which are temples devoted to Shiva. The Nageshwara Jyotirling is located in Aundha Nagnath, Maharshtra. Some individuals believe that the Dwarka Shivalinga in gujrat epresents the Nageshwara Jyotirlinga.",
+        image : Nageshwar,
+        price : "77175",
+},
+
+{
+    id:"10",
+        name :"Baidyanath",
+        info : "The Vaidyanatha temple, also called Baba Vaidyanath Dham is a Hindu Temple dedicated to Lord Shiva. It is located in Deoghar, in the Santhal Parganas division of the state of Jharkhand, India. It is one of the twelve Jyotirlingas, and considered to be the most revered places where Shiva lives.",
+        image : baiyanath,
+        price : "19065",
+},
+
+{
+    id:"11",
+        name :"Rameshwaram",
+        info : "Rameswaram is a town on Pamban Island, in the southeast Indian state of Tamil Nadu. It’s known for Ramanathaswamy Temple, a Hindu pilgrimage site with ornate corridors, huge sculpted pillars and sacred water tanks. Devotees bathe in the waters of Agni Theertham, off the beach east of the temple. Gandamadana Parvatham is a hill with island views. A chakra (wheel) here is said to bear an imprint of Lord Rama’s feet.",
+        image :rameshwaram,
+        price : "61001",
+},
+
+{
+    id:"12",
+        name :"Grishneshwar",
+        info : "Grishneshwar Jyotirlinga Temple, sometimes referred to as the Ghrneshwar or Ghushmeshwar Temple, is one of the shrines dedicated to Lord Shiva that is referenced in the Shiva Purana, and one of the 12 Jyotirlingas.",
+        image : Ghrish,
+        price : "24011",
+},
+
+]
+ export default data;
